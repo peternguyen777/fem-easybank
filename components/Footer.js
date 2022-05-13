74becd88a2f630658cf8295d5091005336aefda0
peternguyen777 <peter.quang.nguyen@gmail.com>
2022-05-13 17:38:55 +1000

    body

---
+++ components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className='w-screen bg-primary'>
+      <h1>hello</h1>
+    </footer>
+  );
+};
+
+export default Footer;
